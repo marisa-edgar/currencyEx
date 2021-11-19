ffd7e1aa53b709a0cfecb20afd8e2ba124ff2715
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-19 11:38:36 -0800

    build coverter

--- src/currency.js
+++ src/currency.js
@@ -15,6 +15,22 @@ export default class currencyService {
     });
   }
   converter(body, number) {
-    const 
+    const ALL = parseFloat(body[0].price);
+    const AMD = parseFloat(body[1].price);
+    const AZN = parseFloat(body[2].price);
+    const BYN = parseFloat(body[3].price);
+    const BAM = parseFloat(body[4].price);
+    const BGN = parseFloat(body[5].price);
+    const HRK = parseFloat(body[6].price);
+    const CZK = parseFloat(body[7].price);
+    const DKK = parseFloat(body[8].price);
+    const GEL = parseFloat(body[9].price);
+    const HUF = parseFloat(body[10].price);
+    const ISK = parseFloat(body[11].price);
+    const CHF = parseFloat(body[12].price);
+    const MDL = parseFloat(body[13].price);
+    const MKD = parseFloat(body[14].price);
+    const NOK = parseFloat(body[15].price);
+    const PLN = parseFloat(body[16].price);
   }
 }
