be9be11eb92159a77465e6d553a24b7dc6769f0d
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-19 09:26:31 -0800

    fix node modules and env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import $ from 'jquery';
+//import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
-
+//import currencyEx from "./js/currency.js";
